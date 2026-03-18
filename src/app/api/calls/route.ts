e8dd3d7d356d0e16a29af80d52075b030d7891b9
tsimpou <kagouras08@gmail.com>
2026-03-18 16:04:57 +0200

    l

--- src/app/api/calls/route.ts
+++ src/app/api/calls/route.ts
@@ -1,6 +1,9 @@
 import { NextResponse } from 'next/server'
 import { createSupabaseServer } from '@/lib/supabaseServer'
 
+export const dynamic = 'force-dynamic'
+export const revalidate = 0
+
 export async function GET() {
   try {
     const supabaseServer = createSupabaseServer()
